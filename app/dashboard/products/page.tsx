5a21a9b726bcd015a8c9510571670174dece0896
Nyambayar1231 <nyambayarlucky@gmail.com>
2024-04-09 17:04:43 +0800

    createProduct

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -17,7 +17,7 @@ export default async function Page() {
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         {/* <Search placeholder="Бараа хайх..." /> */}
-        {/* <CreateProduct /> */}
+        <CreateProduct />
       </div>
       {/* <Suspense
     key={query + currentPage}
